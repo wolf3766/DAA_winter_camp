b49b7d2ea4b4aae7c02d7ff85fd0a6591f14cbb1
Selendra Kumar <76847651+wolf3766@users.noreply.github.com>
2023-01-29 14:28:33 +0530

    Time: 3 ms (85.71%), Space: 5.9 MB (92.86%) - LeetHub

--- 2549-count-distinct-numbers-on-board/2549-count-distinct-numbers-on-board.cpp
+++ 2549-count-distinct-numbers-on-board/2549-count-distinct-numbers-on-board.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
-    int v[101]={0};
-    int cnt=1;
     int distinctIntegers(int n) {
-        v[n]=1;
-        for(int i=2;i<n;i++){
-            if(n%i==1 && v[i]==0){
-               cnt++;
-               distinctIntegers(i);
-            }
-        }return cnt;
+        return max(1,n-1);
     }
 };
