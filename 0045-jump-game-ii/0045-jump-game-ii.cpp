34caec88d9061148fcf2e80d5592203cc2ca1bd2
Selendra Kumar <76847651+wolf3766@users.noreply.github.com>
2023-02-08 15:13:49 +0530

    Time: 20 ms (66.62%), Space: 16.6 MB (56.17%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>dp(n,INT_MAX);
-        dp[0]=0;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<min(n,i+nums[i]+1);j++){
-                dp[j]=min(dp[j],dp[i]+1);
+        int n=nums.size(),ans=0;
+        int cur=0,curjump=0;
+        for(int i=0;i<n-1;i++){
+            curjump=max(curjump,i+nums[i]);
+            
+            if(i==cur){
+                ans++;
+                cur=curjump;
             }
         }
-        return dp[n-1];
+        return ans;
     }
 };
