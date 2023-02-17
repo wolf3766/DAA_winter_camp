d6f1e96001bc4bc7dd958b9f36b9763662552802
Selendra Kumar <76847651+wolf3766@users.noreply.github.com>
2023-02-17 09:59:27 +0530

    Time: 12 ms (41.53%), Space: 9.1 MB (87.92%) - LeetHub

--- 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.cpp
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.cpp
@@ -2,19 +2,18 @@ class Solution {
 public:
     long long pickGifts(vector<int>& gifts, int k) {
         priority_queue<int>pq;
+        long long int sum=0;
         for(auto i:gifts){
             pq.push(i);
+            sum+=i;
         }
         while(k--){
             int tp=pq.top();
             pq.pop();
             pq.push(sqrt(tp));
+            sum-=(tp-sqrt(tp));
         }
-        long long int sum=0;
-        while(!pq.empty()){
-            sum+=pq.top();
-            pq.pop();
-        }
+        
         return sum;
     }
 };
