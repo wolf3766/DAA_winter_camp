522ac3376121ebf3fc1460c3d485f59cbb5dd28d
Selendra Kumar <76847651+wolf3766@users.noreply.github.com>
2023-02-23 09:47:22 +0530

    Time: 8 ms (85.76%), Space: 10.6 MB (96.33%) - LeetHub

--- 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.cpp
+++ 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
+    //two pointer approach
     vector<vector<int>>mergeArrays(vector<vector<int>>&nums1,vector<vector<int>>&nums2){
-        map<int,int>mp;
-        for(auto i:nums1){
-            mp[i[0]]=i[1];
-        }
-        for(auto i:nums2){
-            if(mp.find(i[0])!=mp.end()){
-                mp[i[0]]+=i[1];
+       int i=0,j=0,n1=nums1.size(),n2=nums2.size();
+        vector<vector<int>>ans;
+        while(i<n1 && j<n2){
+            if(nums1[i][0]==nums2[j][0]){
+                ans.push_back({nums1[i][0],nums1[i][1]+nums2[j][1]});
+                i++;
+                j++;
+            }else if(nums1[i][0]<nums2[j][0]){
+                ans.push_back({nums1[i][0],nums1[i][1]});
+                i++;
             }else{
-                mp[i[0]]=i[1];
+                ans.push_back({nums2[j][0],nums2[j][1]});
+                j++;
             }
         }
-        vector<vector<int>>ans;
-        for(auto i:mp){
-            vector<int>temp{i.first,i.second};
-            ans.push_back(temp);
+        while(i<n1){
+            ans.push_back({nums1[i][0],nums1[i][1]});
+            i++;
+        } 
+        while(j<n2){
+            ans.push_back({nums2[j][0],nums2[j][1]});
+            j++;
         }
         return ans;
     }
